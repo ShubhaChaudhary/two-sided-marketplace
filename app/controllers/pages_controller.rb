39e7814d49897633a43433b662d24449a196b39a
Stephen Kel <s.kel@student.qut.edu.au>
2018-11-02 18:32:39 +1100

    added show page, css style and show action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,31 @@
 class PagesController < ApplicationController
+
+  # SEEDED DATA TO BE REPLACED WITH THOSE FROM DATABASE
+  COACH =[{coach: {  user_name: 'John',  name: 'John Doe', 
+          email: 'johndoe@example.com', image: 'john.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Soccer', city: 'Brisbane'}}, 
+      {coach: {  user_name: 'Mary',  name: 'Mary Boiyo', 
+          email: 'maryboiyo@example.com', image: 'mary.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Tenis', city: 'Brisbane'}},
+      {coach: {  user_name: 'Ahmed',  name: 'Ahmed Rasul', 
+          email: 'Ahmedrasulo@example.com', image: 'ahmed.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Gym', city: 'Melbourne'}},
+      {coach: {  user_name: 'Sara',  name: 'Sarah Komeo', 
+          email: 'sarahkomeo@example.com', image: 'sarah.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Gym', city: 'Perth'}}
+  ]
+
   def home
+    # This data is to be replaced with data from the Sports Table
+    @categories = ['Tenis', 'Cricket', 'Rugby', 'Soccer', 'Basketball', 'Gym']
+  end
+
+  def index
+    # This data is to be replaced with data from Coaches Table
+    @coach_count = 0
+    @coaches = COACH
+    
+  end
+
+  def show
+    @coach = COACH
   end
 end
+
+
