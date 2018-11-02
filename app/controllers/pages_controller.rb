4ba5faf8455b90fb426373049f00c9aafaf3acea
Stephen Kel <s.kel@student.qut.edu.au>
2018-11-03 00:09:09 +1100

    added coaches controller and index and show actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,16 @@
 class PagesController < ApplicationController
-
   # SEEDED DATA TO BE REPLACED WITH THOSE FROM DATABASE
-  COACH =[{coach: {  user_name: 'John',  name: 'John Doe', 
-          email: 'johndoe@example.com', image: 'john.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Soccer', city: 'Brisbane'}}, 
-      {coach: {  user_name: 'Mary',  name: 'Mary Boiyo', 
-          email: 'maryboiyo@example.com', image: 'mary.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Tenis', city: 'Brisbane'}},
-      {coach: {  user_name: 'Ahmed',  name: 'Ahmed Rasul', 
-          email: 'Ahmedrasulo@example.com', image: 'ahmed.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Gym', city: 'Melbourne'}},
-      {coach: {  user_name: 'Sara',  name: 'Sarah Komeo', 
-          email: 'sarahkomeo@example.com', image: 'sarah.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Gym', city: 'Perth'}}
-  ]
+  SPORT_TABLE = ['Tenis', 'Cricket', 'Rugby', 'Soccer', 'Basketball', 'Gym']
 
   def home
     # This data is to be replaced with data from the Sports Table
-    @categories = ['Tenis', 'Cricket', 'Rugby', 'Soccer', 'Basketball', 'Gym']
+    @categories = SPORT_TABLE
   end
 
-  def index
-    # This data is to be replaced with data from Coaches Table
-    @coach_count = 0
-    @coaches = COACH
-    
+  def index    
   end
 
   def show
-    @coach = COACH
   end
 end
 
