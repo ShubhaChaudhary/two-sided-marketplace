a8fc0aa5e4bb53e1e1126e711e78a06f881b852c
Stephen Kel <s.kel@student.qut.edu.au>
2018-11-03 15:46:29 +1100

    added links

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,7 +1,6 @@
 class CoachesController < ApplicationController
 
-  # SEEDED DATA TO BE REPLACED WITH THOSE FROM DATABASE
-  
+  # SEEDED DATA TO BE REPLACED WITH THOSE FROM DATABASE  
   COACHES_TABLE =[{coach: {  user_name: 'John',  name: 'John Doe', 
           email: 'johndoe@example.com', image: 'john.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa! Ab, enim!.Voluptates ut nisi animi neque perspiciatis ipsa aspernatur repudiandae similique laboriosam? Molestias recusandae eligendi quasi esse odit! Totam, eligendi culpa', expertise: 'Soccer', city: 'Brisbane'}}, 
       {coach: {  user_name: 'Mary',  name: 'Mary Boiyo', 
@@ -22,4 +21,6 @@ class CoachesController < ApplicationController
     # This data is to be replaced with data from Coaches Table
     @coach = COACHES_TABLE[params[:id].to_i]
   end
+  
+
 end
