3c4a7c23f29bd5ec53e28bcf35b89e1b75cbaf37
=jaouad Sourour <jaouad.pxy@gmail.com>
2018-11-04 17:59:19 +1000

    add a job post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :jobs
+  devise_for :users,
+              path: "",
+              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+              controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+
 end
