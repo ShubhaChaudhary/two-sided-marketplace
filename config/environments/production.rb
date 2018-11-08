0a439996b60f665cb5192b10c8014023e316566f
=jaouad Sourour <jaouad.pxy@gmail.com>
2018-11-08 23:23:48 +1000

    added production setting for the backgound

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -95,6 +95,7 @@ Rails.application.configure do
   config.serve_static_files = true
   config.serve_static_assets = true
 
+    config.assets.digest = true
    config.paperclip_defaults = {
     storage: :s3,
     path: 'class/:attachment/:id/:style/:filename',
