2b2d1bff070e7a2d5c3a09a8265dbc3317a0677d
Stephen Kel <s.kel@student.qut.edu.au>
2018-11-09 20:41:54 +1100

    uncommented before_actions at the trainer's controller

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,6 +2,7 @@ class TrainersController < ApplicationController
   before_action :set_trainer, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
+  # before_action :check_user, only: [:edit, :update, :destroy]
 
  def seller
    @trainers = Trainer.where(user_id: current_user.id)
